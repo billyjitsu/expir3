0e5915b00d8c2a34bb82ef10af811386e31497c1
ropats16 <rohitcpathare@gmail.com>
2022-11-01 12:17:39 +0530

    Add wagmi and ethers and wrap App in wagmi

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import {
+  WagmiConfig,
+  createClient,
+  configureChains,
+  chain,
+  defaultChains,
+} from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.polygonMumbai, chain.polygon],
+  [publicProvider()]
+);
+
+const client = createClient({
+  autoConnect: false,
+  provider,
+  webSocketProvider,
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
