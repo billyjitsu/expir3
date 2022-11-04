2ff2edee3bab910ec4e3c28067f60447093cda23
Ropats <rohitcpathare@gmail.com>
2022-11-04 22:39:16 +0530

    modal ui

--- packages/frontend/pages/dashboard/index.js
+++ packages/frontend/pages/dashboard/index.js
@@ -3,6 +3,7 @@ import { useAccount } from "wagmi";
 // import Dropdown from "../../components/dropdown";
 import Input from "../../components/input";
 import CheckIn from "../../components/checkIn";
+import Modal from "../../components/modal";
 import React from "react";
 
 const index = () => {
@@ -26,6 +27,7 @@ const index = () => {
               Schedule automatic payouts to accounts of your choice as your will
               or as a fall back.
             </p>
+            <Modal />
             <div className="grid grid-cols-5 grid-row-flow gap-4 w-full mx-auto mt-5 ml-5">
               <h3 className="font-semibold">Beneficiary</h3>
               <h3 className="font-semibold">Token</h3>
@@ -33,6 +35,9 @@ const index = () => {
               <h3 className="font-semibold">Amount</h3>
               <h3 className="font-semibold">Delete</h3>
             </div>
+            <button className="text-2xl font-semibold bg-black text-white p-3 self-start mx-5 mt-12 border-none rounded-md">
+              <a href="/dashboard">Add new beneficiary</a>
+            </button>
             {/* <div className="flex flex-row px-40">
               <div className="mt-16 max-w-sm mx-auto">
               </div>
