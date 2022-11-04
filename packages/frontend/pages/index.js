232e53d620964033ba48f35ccd9a34ca690c1810
Ropats <rohitcpathare@gmail.com>
2022-11-04 21:59:44 +0530

    dashboard skeleton

--- packages/frontend/pages/index.js
+++ packages/frontend/pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
         <meta name="description" content="Expir3" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Landing />
     </>
   );
