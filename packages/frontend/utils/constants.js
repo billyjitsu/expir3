6976ca555df0ebb35a934998c2cac10869e0e0b2
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2022-11-14 14:49:53 -0800

    contact updaye

--- packages/frontend/utils/constants.js
+++ packages/frontend/utils/constants.js
@@ -2,7 +2,7 @@ import contractStuff from "../contracts/contract.json";
 
 export const contractAbi = contractStuff.abi;
 
-export const contractAddress = '0x7b6036Af84b4A640388e601d7990e864de78a1E0';
+export const contractAddress = '0x6BeC9836E032e87e6d6323Cf562961bBCE578859';
 export const mockTokenAddress = '0xB7DfFdb405688508e5Ddc593eBAaE64b25a9BB0C';
 export const ERC721MockAddress = '0x316Cd70a2Bbf381A9b0fa326a76E5E6411bAa454';
 export const ERC1155MockAddress = '0xb9f1208fE950eD6b8Ed9202BFA694eaf934eaC64';
